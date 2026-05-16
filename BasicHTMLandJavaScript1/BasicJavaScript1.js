563897bc42a2f8e5b291a60c49960c1cfaa7c3b3
UnstableBrute <abshearcameron@gmail.com>
2026-05-16 16:05:35 -0600

    Changed some code
    Added a few lines, and fixed some bugs.

--- BasicHTMLandJavaScript1/BasicJavaScript1.js
+++ BasicHTMLandJavaScript1/BasicJavaScript1.js
@@ -1,1 +1,7 @@
 alert ("Hello, World! I am an alert box!");
+
+function MyFirstFunction() {
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML = result;
+}
